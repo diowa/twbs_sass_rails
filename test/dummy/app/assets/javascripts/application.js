7f70218dce9ce4bdb2c2e5367d1cf94091375d79
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2013-07-29 11:51:52 +0200

    improved asset folders

--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -10,5 +10,5 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require twbs/bootstrap.js
+//= require twbs/bootstrap
 //= require_tree .
