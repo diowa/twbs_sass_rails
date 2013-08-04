c37c290e6a71219db8f81dab3250d641bb7774a1
Geremia Taglialatela <tagliala.dev@gmail.com>
2013-08-04 15:07:50 +0200

    Rails 3 skips fingerprint when asset has parameters.

--- test/integration/assets_precompile_integration_test.rb
+++ test/integration/assets_precompile_integration_test.rb
@@ -11,16 +11,24 @@ describe "assets precompile in production environment integration" do
   it "provides fontawesome" do
     ['eot', 'svg', 'ttf', 'woff'].each do |fmt|
       font_file = get_asset_name('fontawesome-webfont', fmt)
+      # NOTE: Rails 3 skips fingerprint when asset has parameters.
+      font_file = 'fontawesome-webfont.svg' if Rails.version.start_with?('3')
       visit "/assets/#{font_file}"
       visit "/assets/#{get_asset_name('application', 'css')}"
       page.text.must_include font_file
-      page.text.must_include "#{font_file}?\#iefix" if fmt == 'eot'
+      page.text.must_include "#{font_file}?\#iefix" if fmt == 'eot' && Rails.version.start_with?('4')
     end
   end
 
   it "provides glyphicons" do
     ['eot', 'svg', 'ttf', 'woff'].each do |fmt|
-      visit "/assets/#{get_asset_name('glyphiconshalflings-regular', fmt)}"
+      font_file = get_asset_name('glyphiconshalflings-regular', fmt)
+      # NOTE: Rails 3 skips fingerprint when asset has parameters.
+      font_file = 'glyphiconshalflings-regular.svg' if Rails.version.start_with?('3')
+      visit "/assets/#{font_file}"
+      visit "/assets/#{get_asset_name('application', 'css')}"
+      page.text.must_include font_file
+      page.text.must_include "#{font_file}?\#iefix" if fmt == 'eot' && Rails.version.start_with?('4')
     end
   end
 
