772266eef973a76f6b9c8b860c7fa9fb191f8b8a
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-05-01 10:23:14 +0200

    Refactor method name

--- test/integration/assets_precompile_integration_test.rb
+++ test/integration/assets_precompile_integration_test.rb
@@ -7,14 +7,14 @@ describe 'assets precompile in production environment integration' do
   system 'cd test/dummy && RAILS_ENV=production rake assets:clean assets:precompile'
 
   it 'provides Respond.js' do
-    visit "/assets/#{get_asset_name('respond', 'js')}"
+    visit "/assets/#{asset_file('respond', 'js')}"
   end
 
   it 'provides Font Awesome' do
     %w[eot svg ttf woff woff2].each do |fmt|
-      font_file = get_asset_name('fontawesome-webfont', fmt)
+      font_file = asset_file('fontawesome-webfont', fmt)
       visit "/assets/#{font_file}"
-      visit "/assets/#{get_asset_name('application', 'css')}"
+      visit "/assets/#{asset_file('application', 'css')}"
       page.text.must_include font_file
       page.text.must_include "#{font_file}?\#iefix" if fmt == 'eot'
     end
@@ -22,39 +22,39 @@ describe 'assets precompile in production environment integration' do
 
   it 'provides Glyphicons' do
     %w[eot svg ttf woff woff2].each do |fmt|
-      font_file = get_asset_name('glyphicons-halflings-regular', fmt)
+      font_file = asset_file('glyphicons-halflings-regular', fmt)
       visit "/assets/#{font_file}"
-      visit "/assets/#{get_asset_name('application', 'css')}"
+      visit "/assets/#{asset_file('application', 'css')}"
       page.text.must_include font_file
       page.text.must_include "#{font_file}?\#iefix" if fmt == 'eot'
     end
   end
 
   it 'overrides Bootstrap variables' do
-    visit "/assets/#{get_asset_name('application', 'css')}"
+    visit "/assets/#{asset_file('application', 'css')}"
     page.text.must_include 'color:#d10d10'
     page.text.must_include 'color:#89090a' # automatically generated hover
   end
 
   it 'overrides Bootstrap variables in user stylesheets' do
-    visit "/assets/#{get_asset_name('application', 'css')}"
+    visit "/assets/#{asset_file('application', 'css')}"
     page.text.must_include '.test-class{color:#d10d10}'
     page.text.must_include '.test-hover-class{color:#89090a}'
   end
 
   it 'allows to import mixins' do
-    visit "/assets/#{get_asset_name('application', 'css')}"
+    visit "/assets/#{asset_file('application', 'css')}"
     page.text.must_include '.subfolder{width:0;height:0}'
   end
 
   it 'sets Sass precision to 10' do
-    visit "/assets/#{get_asset_name('application', 'css')}"
+    visit "/assets/#{asset_file('application', 'css')}"
     page.text.must_include '0.1111111111em'
   end
 
   private
 
-  def get_asset_name(asset_name, asset_ext)
-    Dir.glob("test/dummy/public/assets/#{asset_name}-*.#{asset_ext}").first.split('/').last
+  def asset_file(file_name, file_ext)
+    Dir.glob("test/dummy/public/assets/#{file_name}-*.#{file_ext}").first.split('/').last
   end
 end
